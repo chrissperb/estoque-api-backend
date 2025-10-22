3a7e3e106e69579999f603ce2f2d1d51e4c5a8b4
Christian Sperb <chrissperb@gmail.com>
2025-10-21 21:10:58 -0300

    chore: atualização do caminho para conectar o frontend.

--- src/main/java/br/com/borbolelala/estoqueapi/config/WebConfig.java
+++ src/main/java/br/com/borbolelala/estoqueapi/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://borbolelala-estoque-api.azurewebsites.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
